4853a359d409e1392cbf500ab59271916866369b
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-04-24 02:51:02 +0200

    ran `create-react-app` & initialized global color pallet

---
+++ src/values/colors.js
@@ -0,0 +1,12 @@
+const COLORS = {
+    primary: '#E8999C',
+    primaryLight: '#EAB1A0',
+    secondary: '#EDCCAD',
+    tertiary: '#0000FF',
+    white: '#FFFFFF',
+    black: '#000000',
+    background: '#E7E3BE',
+    yellow: '#E1D59B',
+    textColor: '#000000',
+
+};
