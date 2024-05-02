fcd60539cf4b550b0c068c328582d9ef5151d3fe
mowaell <mwael0304@gmail.com>
2024-05-02 17:25:12 +0300

    fixed a bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -14,11 +13,9 @@ import AdminHome from './AdminHome';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
     <PasswordManagement/>
   </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
