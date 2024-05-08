2f1171ddfd8f5be762958e3b6abd2767493f38cc
mariemmobarak <mariem.mobarak@student.edu.eg>
2024-05-08 20:08:46 +0300

    i finalized my filtering and changed all of the requests to be forms and not text boxes and adjusted the images

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import React, { useEffect } from 'react';
 import './App.css';
-
-//import DetailsOfClothesToBeDonated from './components/DetailsOfClothesToBeDonated';
-import { Divider } from 'antd';
-import NotificationForChosenPosts from './components/NotificationForChosenPosts';
-//import DetailsOfMedicalSuppToBeDonated from './components/DetailsOfMedicalSuppToBeDonated';
-//import DetailsOfBooksToBeDonated from './components/DetailsOfBooksToBeDonated';
-//import DetailsOfClothesToBeDonated from './components/DetailsOfClothesToBeDonated';
-//import DetailsOfFoodToBeDonated from './components/DetailsOfFoodToBeDonated';
-//import DetailsOfMedicalSuppToBeDonated from './components/DetailsOfMedicalSuppToBeDonated';
-//import DetailsOfStationaryToBeDonated from './components/DetailsOfStationaryToBeDonated';
-import DetailsOfToysToBeDonated from './components/DetailsOfToysToBeDonated';
-//import OrgCoordinateDonationPickup from './components/OrgCoordinateDonationPickup';
-//import ViewDetailsOfFulfilledPosts from './components/ViewDetailsOfFulfilledPosts';
-//import ViewDonorDetailsForFulfilledPosts from './components/ViewDonorDetailsForFulfilledPosts';
-//import NotificationForDriverArrival from './components/NotificationForDriverArrival';
 import NavBar from './components/NavBar';
-
+ import ViewDonationRequests from './components/ViewDonationRequests';
+import ViewEstimatedArrival from './components/ViewEstimatedArrival';
+  import CreatePost from './components/CreatePost';
+//  import ViewRequestImage from './components/ViewRequestImage.jpg';
+ import React from 'react';
+  import {Button} from 'antd';
+  import  { useState} from 'react';
 //import FulfilledDonations from './components/FulfilledDonations';
 //import DetailsOfToysToBeDonated from './components/DetailsOfToysToBeDonated';
 
 
 function App() {
+ 
   return (
+  
+
     <div className="App">
     <NavBar />
+    {/* <CreatePost/> */}
+    {/* <CreatePost/> */}
+    {/* <ViewDonationRequests/>  */}
+
+  
     
+    <header className="App-header"></header>
   </div>
 );
 }
