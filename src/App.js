bc32f0e35ac816861db5c80ed680f7ee993b1a71
Amr Hegazy <68474553+Amr-Hegazy1@users.noreply.github.com>
2024-05-12 22:43:13 +0300

    forced user to log in if not logged in

--- src/App.js
+++ src/App.js
@@ -1,21 +1,139 @@
-import React, { useEffect } from 'react';
-import './App.css';
+import {React, useEffect} from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import NavBar from './components/NavBar';
-import NotificationsOrg from './components/AdminHomePage/TopNav/NotificationsOrg';
-import DeliveryPersonSignUp from './components/DeliveryPersonSignUp';
+import VerifyCodePage from './components/Verify';
+import AdminHome from './components/AdminHomePage/AdminHome';
+
+import PasswordManagement from './PasswordManagement';
+import OrgCoordinateDonationPickup from './components/OrgCoordinateDonationPickup';
+import DetailsOfMedicalSuppToBeDonated from './components/DetailsOfMedicalSuppToBeDonated';
+import DetailsOfToysToBeDonated from './components/DetailsOfToysToBeDonated';
+import DetailsOfBooksToBeDonated from './components/DetailsOfBooksToBeDonated';
+import DetailsOfStationaryToBeDonated from './components/DetailsOfStationaryToBeDonated';
+import DetailsOfFoodToBeDonated from './components/DetailsOfFoodToBeDonated';
+import ViewDonorDetailsForFulfilledPosts from './components/ViewDonorDetailsForFulfilledPosts';
+import { CookiesProvider } from 'react-cookie';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { HelmetProvider } from 'react-helmet-async';
+import { Suspense } from 'react';
+import DetailsOfClothesToBeDonated from './components/DetailsOfClothesToBeDonated';
+
+import OrganizationRequestInfo from './components/OrganizationRequestInfo';
+import OrganizationDetails from './components/OrganizationDetails';
+
+import RequestClothes from './components/RequestClothes';
+import RequestBooks from './components/RequestBooks.';
+import RequestFood from './components/RequestFood';
+import RequestMedicalSupplies from './components/RequestMedicalSup';
+import RequestStationary from './components/RequestStationary';
+import RequestToys from './components/RequestToys';
+import RequestBloodDonations from './components/RequestBloodDonations';
+import RequestTeaching from './components/RequestTeaching';
+import RequestMedicalCases from './components/RequestMedicalCases';
+import ViewDonationRequests from './components/ViewDonationRequests';
+import Homepage from './components/Homepage';
+import Signup from './components/Signup';
+import FulfilledDonations from './components/FulfilledDonations';
+import SchedulePickup from './components/SchedulePickup';
+import DonorMyDonations from './components/DonorMyDonations';
+import DonorMyDonationDetails from './components/DonorMyDonationDetails';
+import Dashboard from './components/DeliveryPerson/Dashboard';
+import DeliveryDonationDetails from './components/DeliveryPerson/DeliveryDonationDetails';
+import { ConfigProvider } from 'antd';
+import { createTheme, ThemeProvider } from '@mui/material';
+import RequestInfoDoctor from './components/RequestInfoDoctor';
+import TeacherDetails from './components/TeacherDetails';
+import DoctorDetails from './components/DoctorDetails';
+import ViewDetailsOfFulfilledPosts from './components/ViewDetailsOfFulfilledPosts';
+
+
+import Login from './Login';
+import RequestInfoTeacher from './components/RequestInfoTeacher';
+import DonorProfile from './components/DonorProfile';
+import OrganizationProfile from './components/OrganizationProfile';
+import AdminProfile from './components/AdminProfile';
+import DeliveryPersonProfile from './components/DeliveryPersonProfile';
+import AdminFilterOrganization from './components/AdminFilterOrganization';
+import OrganizationRegisteredDetails from './components/OrganizationRegisteredDetails';
+import DoctorRegisteredInfo from './components/DoctorRegisteredInfo';
+import TeacherRegisteredInfo from './components/TeacherRegisteredInfo';
+import { useCookies } from 'react-cookie';
 
 
 
 function App() {
+
+  const [cookies, setCookie] = useCookies();
+
+  useEffect(() => {
+    if (cookies['user_type'] === undefined || cookies['user_type'] === null || cookies['user_type'] === '') {
+      if (window.location.pathname !== '/login' && window.location.pathname !== '/signup' && window.location.pathname !== '/') {
+        window.location.href = '/login';
+      }
+    }
+  }, [cookies]);
+
   return (
-    <div className="App">
-     <NotificationsOrg/>
-    <NavBar />
-    <DeliveryPersonSignUp/>
- 
-  
-  </div>
+    <HelmetProvider>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Suspense>
+          <Routes>
+            <Route index element={<Homepage />} />
+            <Route path="/donateClothes" element={<DetailsOfClothesToBeDonated />} />
+            <Route path="/view-org-request-info" element={<OrganizationRequestInfo />} />
+            <Route path="/view-org-submission-info" element={<OrganizationDetails />} />
+            <Route path="/view-doctor-submission-info" element={<DoctorDetails />} />
+            <Route path='/view-teacher-submission-info' element={<TeacherDetails />} />
+            <Route path="/view-org-registered-info" element={<OrganizationRegisteredDetails />} />
+            <Route path='view-doctor-registered-info' element={<DoctorRegisteredInfo />} />
+            <Route path="/view-teacher-registered-info" element={<TeacherRegisteredInfo />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/AdminHome" element={<AdminHome />} />
+            <Route path="/ChangePassword" element={<PasswordManagement />} />
+            <Route path="/Verify" element={<VerifyCodePage />} />
+            <Route path="/RequestClothes" element={<RequestClothes />} />
+            <Route path="/RequestBooks" element={<RequestBooks />} />
+            <Route path="/RequestFood" element={<RequestFood />} />
+            <Route path="/RequestMedicalSupplies" element={<RequestMedicalSupplies />} />
+            <Route path="/RequestStationary" element={<RequestStationary />} />
+            <Route path="/RequestToys" element={<RequestToys />} />
+            <Route path="/RequestBloodDonations" element={<RequestBloodDonations />} />
+            <Route path="/RequestTeaching" element={<RequestTeaching />} />
+            <Route path="/RequestMedicalCases" element={<RequestMedicalCases />} />
+            <Route path="/ViewDonationRequests" element={<ViewDonationRequests />} />
+            <Route path="/donateMedicalSupplies" element={<DetailsOfMedicalSuppToBeDonated />} />
+            <Route path="/donateToys" element={<DetailsOfToysToBeDonated />} />
+            <Route path="/donateBooks" element={<DetailsOfBooksToBeDonated />} />
+            <Route path="/donateStationary" element={<DetailsOfStationaryToBeDonated />} />
+            <Route path="/donateFood" element={<DetailsOfFoodToBeDonated />} />
+            <Route path="/coordinatePickup" element={<OrgCoordinateDonationPickup />} />
+            <Route path="/donorDetails" element={<ViewDonorDetailsForFulfilledPosts />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/fullfilledDonations" element={<FulfilledDonations />} />
+            <Route path="/schedulePickup" element={<SchedulePickup />} />
+            <Route path="/DonorProfile" element={<DonorProfile />} />
+            <Route path="/OrganizationProfile" element={<OrganizationProfile />} />
+            <Route path="/AdminProfile" element={<AdminProfile />} />
+            <Route path="/DeliveryPersonProfile" element={<DeliveryPersonProfile />} />
+            <Route path="/donorViewAllDonations" element={<DonorMyDonations />} />
+            <Route path="/donorMyDonationDetails" element={<DonorMyDonationDetails />} />
+            <Route path="/delieveyPersonDashboard" element={<Dashboard />} />
+            <Route path='/view-doctor-request-info' element={<RequestInfoDoctor />} />
+            <Route path='/view-teacher-request-info' element={<RequestInfoTeacher />} />
+            <Route path="/deliveryDonationDetails" element={<DeliveryDonationDetails />} />
+            <Route path='/homePage' element={<Homepage />} />
+            <Route path="/fulfilledPostDetails" element={<ViewDetailsOfFulfilledPosts />} />
+            <Route path='/registeredOrganizations' element={<AdminFilterOrganization />} />
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </CookiesProvider>
+  </HelmetProvider>
 );
 }
 
